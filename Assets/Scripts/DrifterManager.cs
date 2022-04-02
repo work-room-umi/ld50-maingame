b0f98d431ffa3340220dec09145c28ea0c3872d9
Trs <trsnium@gmail.com>
2022-04-02 21:00:53 +0900

    add driftermanager script

---
+++ Assets/Scripts/DrifterManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrifterManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ship;
+    [SerializeField]
+    private float deleteDist;
+    [SerializeField]
+    private float spawnDist;
+    [SerializeField]
+    private int maxDrifterNum;
+    [SerializeField]
+    private string drifterPrefab;
+
+    private List<GameObject> managedDrifters;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        managedDrifters = new List<GameObject>();
+    }
+
+    private Vector3 generateRandomBasePosition()
+    {
+        float generateComponent()
+        {
+            float c = Random.Range(spawnDist, deleteDist);
+            if (Random.value < .5)
+            {
+                return c;
+            }
+            else
+            {
+                return -c;
+            }
+        };
+        return new Vector3(generateComponent(), 0, generateComponent());
+    }
+
+    void GC(Vector3 shipPosition)
+    {
+        foreach(GameObject drifter in managedDrifters)
+        {
+                Vector3 position = drifter.transform.position;
+                float dist = Vector3.Distance(position, shipPosition);
+                if( deleteDist < dist )
+                {
+                    managedDrifters.Remove(drifter);
+                    Destroy(drifter);
+                }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 shipPosition = ship.transform.position;
+        //GC(shipPosition);
+        
+        if (managedDrifters.Count<maxDrifterNum)
+        {
+            // 漂流物の初期位置を計算する
+            Vector3 basePosition = generateRandomBasePosition();
+            Vector3 position = shipPosition + basePosition;
+
+            GameObject prefab = (GameObject)Resources.Load(drifterPrefab);
+            GameObject drifter = Instantiate (prefab, position, Quaternion.identity);
+            managedDrifters.Add(drifter);
+        }
+    }
+}
