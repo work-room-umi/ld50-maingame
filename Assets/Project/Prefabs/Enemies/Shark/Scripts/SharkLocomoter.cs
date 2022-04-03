77e14068e7ceb8585a2d7407294f06692da0611d
tanitta <ttata.trit@gmail.com>
2022-04-03 21:36:06 +0900

    Fixed.

--- Assets/Project/Prefabs/Enemies/Shark/Scripts/SharkLocomoter.cs
+++ Assets/Project/Prefabs/Enemies/Shark/Scripts/SharkLocomoter.cs
@@ -5,45 +5,45 @@ using UnityEngine.AI;
 
 public class SharkLocomoter : MonoBehaviour
 {
-	NavMeshAgent _agent;
+    NavMeshAgent _agent;
 
-	[SerializeField]
-	Transform _target;
+    [SerializeField]
+    Transform _target;
 
-	[SerializeField]
-	float _speedFreq = 2f;
+    [SerializeField]
+    float _speedFreq = 2f;
 
-	[SerializeField]
-	float _speedAmp = 1f;
+    [SerializeField]
+    float _speedAmp = 1f;
 
-	float _defaultSpeed;
+    float _defaultSpeed;
 
-	[SerializeField]
-	float _lifeTime = 0f;
+    [SerializeField]
+    float _lifeTime = 0f;
 
-	public void SetTarget(Transform target)
-	{
-		_target = target;
-	}
+    public void SetTarget(Transform target)
+    {
+        _target = target;
+    }
 
-	void Start()
-	{
-		_agent = GetComponent<NavMeshAgent>();
-		_defaultSpeed = _agent.speed;
-	}
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _defaultSpeed = _agent.speed;
+    }
 
-	void Update()
-	{
-		_agent.destination = _target.position;
-		_agent.speed = _defaultSpeed + Mathf.Sin(Time.time * _speedFreq) * _speedAmp;
-		var targetDir = (_agent.destination - transform.position).normalized;
-		_agent.speed *= Mathf.Clamp01(Vector3.Dot(targetDir, transform.forward));
+    void Update()
+    {
+        _agent.destination = _target.position;
+        _agent.speed = _defaultSpeed + Mathf.Sin(Time.time * _speedFreq) * _speedAmp;
+        var targetDir = (_agent.destination - transform.position).normalized;
+        _agent.speed *= Mathf.Clamp01(Vector3.Dot(targetDir, transform.forward));
 
-		_lifeTime -= Time.deltaTime;
+        _lifeTime -= Time.deltaTime;
 
-		if (_lifeTime < 0f)
-		{
-			Destroy(gameObject);
-		}
-	}
+        if (_lifeTime < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
