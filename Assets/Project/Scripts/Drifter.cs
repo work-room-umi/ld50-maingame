a771c73f50ee585b498590e350b6bbf3987f7584
Trs <trsnium@gmail.com>
2022-04-03 19:02:59 +0900

    move drift object

--- Assets/Project/Scripts/Drifter.cs
+++ Assets/Project/Scripts/Drifter.cs
@@ -11,6 +11,8 @@ public class Drifter : MonoBehaviour
 	[SerializeField]
 	float _amp;
 	[SerializeField]
+	float _noiseScale=0;
+	[SerializeField]
 	GameObject _wave;
 
 	// Start is called before the first frame update
@@ -19,31 +21,26 @@ public class Drifter : MonoBehaviour
 		_child = transform.GetChild(0);
 	}
 
+	Vector3 GetNoiseDir(Vector3 pos)
+	{
+		float noise = Mathf.PerlinNoise(pos.x, pos.z);
+		Vector3 noiseDir = new Vector3(Mathf.Cos(noise * Mathf.PI), Mathf.Sin(noise * Mathf.PI), Mathf.Sin(noise * Mathf.PI));
+		Vector3 dir = Vector3.Normalize(noiseDir);
+		return dir;
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
 		Waves wave = _wave.GetComponent<Waves>();
-		_child.rotation = Quaternion.identity;
 		float height = wave.GetHeight(_child.position);
 		Vector3 normal = wave.GetNormal(_child.position);
+		Vector3 moveDir = GetNoiseDir(_child.position) * _noiseScale;
+
+		_child.rotation = Quaternion.identity;
 		Quaternion targetRotation = Quaternion.FromToRotation(_child.up, normal);
 		_child.rotation = targetRotation;
-		_child.position = new Vector3(_child.position.x, height + Mathf.Sin(Time.time*_freq)*_amp, _child.position.z);
-	}
+		_child.position = new Vector3(_child.position.x + moveDir.x, height + Mathf.Sin(Time.time*_freq)*_amp, _child.position.z + moveDir.z);
 
-	Vector3 SmoothedNormal(RaycastHit aHit)
-	{
-		var MC = aHit.collider as MeshCollider;
-		if (MC == null)
-			return aHit.normal;
-		var M = MC.sharedMesh;
-		var normals = M.normals;
-		var indices = M.triangles;
-		var N0 = normals[indices[aHit.triangleIndex * 3 + 0]];
-		var N1 = normals[indices[aHit.triangleIndex * 3 + 1]];
-		var N2 = normals[indices[aHit.triangleIndex * 3 + 2]];
-		var B = aHit.barycentricCoordinate;
-		var localNormal = (B[0] * N0 + B[1] * N1 + B[2] * N2).normalized;
-		return MC.transform.TransformDirection(localNormal);
 	}
 }
