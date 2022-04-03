f7754c0cca2f28130cb424850d48e409f7ff6de7
terabyteF <kamose_monera@yahoo.co.jp>
2022-04-03 19:35:44 +0900

    りやさんマージ前

--- Assets/Project/Scripts/MoveScene.cs
+++ Assets/Project/Scripts/MoveScene.cs
@@ -1,14 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace umi.ld50
 {
     public class MoveScene : MonoBehaviour
     {
         public string sceneName;
+        public float waitFade = 0.5f;
+        public Sprite fadeImage;
+        public bool isFadeIn = false;
+        public bool isFadeOut = false;
+
         public void OnClickButton()
         {
+            StartCoroutine(DelayCoroutine());
+        }
+
+        private IEnumerator DelayCoroutine()
+        {
+            //待つ
+            yield return new WaitForSeconds(waitFade);
+            
+            //n秒かけて切り替わる(ディゾルブ)
+            Fade();
+        }
+
+        private void FadeIn()
+        {
+            isFadeIn = true;
+        }
+
+        private void FadeOut()
+        {
+            isFadeOut = true;
+        }
+        private void Fade()
+        {
+            if (isFadeIn)
+            {
+
+            }
+            else if(isfadeout)
+            {
+                
+            }
             SceneManager.LoadScene(sceneName);
+
         }
     }
+
 }
