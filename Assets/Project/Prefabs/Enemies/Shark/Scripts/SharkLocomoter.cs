9e8591b853c8bf50917b5e4af95b1f685d7ba1af
tanitta <ttata.trit@gmail.com>
2022-04-03 20:41:57 +0900

    サメが寿命で消えるようにした。

--- Assets/Project/Prefabs/Enemies/Shark/Scripts/SharkLocomoter.cs
+++ Assets/Project/Prefabs/Enemies/Shark/Scripts/SharkLocomoter.cs
@@ -10,6 +10,17 @@ public class SharkLocomoter : MonoBehaviour
 	[SerializeField]
 	Transform _target;
 
+	[SerializeField]
+	float _speedFreq = 2f;
+
+	[SerializeField]
+	float _speedAmp = 1f;
+
+	float _defaultSpeed;
+
+	[SerializeField]
+	float _lifeTime = 0f;
+
 	public void SetTarget(Transform target){
 		_target = target;
 	}
@@ -17,10 +28,20 @@ public class SharkLocomoter : MonoBehaviour
     void Start()
     {
 		_agent = GetComponent<NavMeshAgent>();
+		_defaultSpeed = _agent.speed;
     }
 
     void Update()
     {
 		_agent.destination = _target.position;
+		_agent.speed = _defaultSpeed + Mathf.Sin(Time.time*_speedFreq)*_speedAmp;
+		var targetDir = (_agent.destination - transform.position).normalized;
+		_agent.speed *= Mathf.Clamp01(Vector3.Dot(targetDir, transform.forward));
+
+		_lifeTime -= Time.deltaTime;
+
+		if(_lifeTime<0f){
+			Destroy(gameObject);
+		}
     }
 }
