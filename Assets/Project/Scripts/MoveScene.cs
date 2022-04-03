c482f21f222ebbff9ed6276c6e4952684f4107b2
terabyteF <kamose_monera@yahoo.co.jp>
2022-04-03 22:40:26 +0900

    sceneNameバグる

--- Assets/Project/Scripts/MoveScene.cs
+++ Assets/Project/Scripts/MoveScene.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -6,11 +7,16 @@ namespace umi.ld50
 {
     public class MoveScene : MonoBehaviour
     {
-        public string sceneName;
+        private static Canvas fadeCanvas;
+        private static Image fadeImage;
+        private static float alpha = 0.0f;
+
+        public string nextScene;
         public float waitFade = 0.5f;
-        public Sprite fadeImage;
-        public bool isFadeIn = false;
-        public bool isFadeOut = false;
+        public float fadeinTime = 0.5f;
+        public float fadeoutTime = 0.5f;
+        private static bool isFadeIn = false;
+        private static bool isFadeOut = false;
 
         public void OnClickButton()
         {
@@ -23,31 +29,76 @@ namespace umi.ld50
             yield return new WaitForSeconds(waitFade);
             
             //n秒かけて切り替わる(ディゾルブ)
-            Fade();
+            FadeOut();
+        }
+        static void Init()
+        {
+            //フェード用のCanvas生成
+            GameObject FadeCanvasObject = new GameObject("CanvasFade");
+            fadeCanvas = FadeCanvasObject.AddComponent<Canvas>();
+            FadeCanvasObject.AddComponent<GraphicRaycaster>();
+            fadeCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            FadeCanvasObject.AddComponent<MoveScene>();
+
+            //最前面になるよう適当なソートオーダー設定
+            fadeCanvas.sortingOrder = 100;
+
+            //フェード用のImage生成
+            fadeImage = new GameObject("ImageFade").AddComponent<Image>();
+            fadeImage.transform.SetParent(fadeCanvas.transform, false);
+            fadeImage.rectTransform.anchoredPosition = Vector3.zero;
+
+            //Imageサイズは適当に大きく設定してください
+            fadeImage.rectTransform.sizeDelta = new Vector2(9999, 9999);
         }
 
         private void FadeIn()
         {
+            if (fadeImage == null) Init();
+            fadeImage.color = Color.black;
             isFadeIn = true;
         }
 
         private void FadeOut()
         {
+            if (fadeImage == null) Init();
+            fadeImage.color = Color.clear;
+            fadeCanvas.enabled = true;
             isFadeOut = true;
         }
-        private void Fade()
+
+        // void Start()
+        // {
+        //     FadeIn();
+        // }
+        void Update()
         {
             if (isFadeIn)
             {
+                alpha -= Time.deltaTime / fadeinTime;
 
+                if (alpha <= 0.0f)
+                {
+                    isFadeIn = false;
+                    alpha = 0.0f;
+                    fadeCanvas.enabled = false; 
+                }
+
+                fadeImage.color = new Color(0.0f, 0.0f, 0.0f, alpha);
             }
             else if(isFadeOut)
             {
+                alpha += Time.deltaTime / fadeoutTime;
 
+                if(alpha >= 1.0f)
+                {
+                    isFadeOut = false;
+                    alpha = 1.0f;
+
+                    SceneManager.LoadScene(nextScene);
+                }
+                fadeImage.color = new Color(0.0f, 0.0f, 0.0f, alpha);
             }
-            SceneManager.LoadScene(sceneName);
-
         }
     }
-
 }
