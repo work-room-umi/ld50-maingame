2953c8942de67743984b555c24e1ccd380ee3288
tanitta <ttata.trit@gmail.com>
2022-04-04 21:42:52 +0900

    Fix enemy spawn.

--- Assets/Project/Scripts/EnemyManagerScriptableObject.cs
+++ Assets/Project/Scripts/EnemyManagerScriptableObject.cs
@@ -8,4 +8,6 @@ public class EnemyManagerScriptableObject : ScriptableObject
     public List<GameObject> prefabs;
     public int numberOfPrefabsToCreate;
     public float spawnDist;
+    [Tooltip("スポーンの間隔の秒数[s]")]
+    public float spawnInterval = 1f;
 }
