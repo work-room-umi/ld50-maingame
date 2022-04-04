9ad39c55c5ec54720912464b9e1ceec9ab4a1fc9
koiusa <71818379+koiusa@users.noreply.github.com>
2022-04-04 23:40:56 +0900

    イベントメソッド名の変更をマージ

--- Assets/Project/Scripts/SEShark.cs
+++ Assets/Project/Scripts/SEShark.cs
@@ -19,7 +19,7 @@ namespace umi.ld50
         // Start is called before the first frame update
         void Start()
         {
-            attack.OnDonAttacked += PlaySound;
+            attack.OnAttacked += PlaySound;
 
         }
 
