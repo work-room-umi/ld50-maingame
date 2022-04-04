ae71608591ec88a3f457bbf4ba04694f7c47570c
Trs <trsnium@gmail.com>
2022-04-04 16:25:50 +0900

    add enemymanager

--- Assets/Project/Scripts/EnemyManager.cs
+++ Assets/Project/Scripts/EnemyManager.cs
@@ -8,7 +8,8 @@ namespace umi.ld50 {
     {
         [SerializeField]
         private PlayerShip _ship;
-        EnemyManagerScriptableObject enemyManagerValues;
+        [SerializeField]
+        private EnemyManagerScriptableObject enemyManagerValues;
         int instanceNumber = 1;
 
         // Start is called before the first frame update
@@ -30,13 +31,19 @@ namespace umi.ld50 {
                 // 原点からの距離が一定の場合のみスポーン
                 Vector3 shipPosition = _ship.gameObject.transform.position;
                 float distanceFromOrigin = Vector3.Distance(shipPosition, new Vector3(0, 0, 0));
-                if (distanceFromOrigin > enemyManagerValues.spawnDist && instanceNumber < enemyManagerValues.numberOfPrefabsToCreate){
+                if (distanceFromOrigin > enemyManagerValues.spawnDist && this.transform.childCount < enemyManagerValues.numberOfPrefabsToCreate){
                     int index = UnityEngine.Random.Range(0, enemyManagerValues.prefabs.Count);
                     GameObject prefab = enemyManagerValues.prefabs[index];
 
                     // スポーン位置を計算
-                    Vector3 spanwPosition = new Vector3(0, 0, 0) - shipPosition;
-                    GameObject driftObj = Instantiate (prefab, spanwPosition, Quaternion.identity);
+                    Vector3 spanwPosition = -shipPosition;
+                    GameObject enemy = Instantiate (prefab, spanwPosition, Quaternion.identity);
+                    enemy.transform.parent = gameObject.transform;
+
+                    // Sharklocomoterにshipを追従させる
+                    SharkLocomoter sharkLocomoter = enemy.GetComponent<SharkLocomoter>();
+                    sharkLocomoter.SetTarget(_ship.gameObject.transform);
+                    instanceNumber++;
                 }
         }
     }
