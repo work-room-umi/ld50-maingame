9a2be531a3f240ef3096f99c15e318e513399e7c
koiusa <71818379+koiusa@users.noreply.github.com>
2022-04-05 07:17:27 +0900

    VFXEmitterComponentを実装

---
+++ Assets/Project/Scripts/VFXEmitterComponent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace umi.ld50
+{
+    public class VFXEmitterComponent : MonoBehaviour
+    {
+        [SerializeField] GameObject prefab;
+
+        [SerializeField] float lifeTime;
+
+        private GameObject instance;
+        public void EmitVFX()
+        {
+            if (prefab == null)
+            {
+                return;
+            }
+            instance = Instantiate(prefab, this.transform.position, Quaternion.identity); 
+        }
+        void PlayParticle()
+        {
+            StartCoroutine(DelayAction(lifeTime));
+        }
+        IEnumerator DelayAction(float lifeTime)
+        {
+            yield return new WaitForSeconds(lifeTime);
+            Destroy(this.gameObject);
+        }
+    }
+}
